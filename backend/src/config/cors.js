74b4d2426e9edb7657ca8bb5a8268776686d2958
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2020-10-27 08:29:11 -0300

    Inclusão do suporte ao CORS.

---
+++ backend/src/config/cors.js
@@ -0,0 +1,6 @@
+module.exports = function (req, res, next) {
+    res.header('Allow-Access-Allow-Origin', '*');
+    res.header('Allow-Access-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow-Access-Allow-Headers', 'Origin, X-Requested-Width, Content-Type, Accept');
+    next();
+};
