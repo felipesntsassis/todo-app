6d2fbbb06069ab08e65c2a5ffb4073655df59474
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2020-10-30 07:58:27 -0300

    Criação dos componentes Todo e About.

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,8 +3,12 @@ import 'modules/font-awesome/css/font-awesome.min.css';
 
 import React from 'react';
 
+import About from '../about/about';
+import Todo from '../todo/todo';
+
 export default props => (
     <div className="container">
-        <h1>Teste</h1>
+        <Todo />
+        <About />
     </div>
 );
