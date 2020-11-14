90c51a26aa11b5bf82e03a743e6a61bdfb159bcd
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2020-11-14 11:27:22 -0300

    Correção das configurações de CORS.

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -1,6 +1,6 @@
 module.exports = function (req, res, next) {
-    res.header('Allow-Access-Allow-Origin', '*');
-    res.header('Allow-Access-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow-Access-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 };
