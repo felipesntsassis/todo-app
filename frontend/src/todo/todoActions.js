eca24845cea8a939c8e80c9fd3ff2aae11da708a
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2021-01-17 13:04:47 -0300

    Inclusão do recurso redux-thunk para garantir as requisições assíncronas,

--- frontend/src/todo/todoActions.js
+++ frontend/src/todo/todoActions.js
@@ -7,19 +7,15 @@ export const changeDescription = event => ({
 });
 
 export const add = (description) => {
-	const request = axios.post(`${URL}`, { description });
-
-	return {
-		type: 'TODO_ADDED',
-		payload: request,
-	};
-};
+    return dispatch => {
+        axios.post(URL, { description })
+            .then(resp => dispatch({ type: 'TODO_ADDED', payload: resp.data }))
+            .then(resp => dispatch(search()));
+    };
+}
 
 export const search = () => {
     const request = axios.get(`${URL}?sort=-createdAt`);
 
-    return {
-        type: 'TODO_SEARCHED',
-        payload: request
-    };
+    return { type: 'TODO_SEARCHED', payload: request };
 };
